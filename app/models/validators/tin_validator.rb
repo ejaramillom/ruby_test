f4f493ede7e412583a739a14208ed915841948ac
ejaramillom <emmanuel@knakai.com>
2025-02-20 16:51:40 -0500

    feat(PT1): Fixed inclusion of validations for each country

--- app/models/validators/tin_validator.rb
+++ app/models/validators/tin_validator.rb
@@ -1,75 +1,76 @@
 # frozen_string_literal: true
 
 class TinValidator < ActiveModel::Validator
-  COUNTRIES = %s(au ca in)
   def validate(record)
-    unless COUNTRIES.include?(record.country.downcase)
-      return records.error.add 'Country does not exist for TIN generation'
+    @normalized_number = record.number.gsub(' ', '')
+
+    case record.country.downcase
+    when 'au' then validate_au(record)
+    when 'ca' then validate_ca(record)
+    when 'in' then validate_in(record)
+    else record.errors.add 'Country not found for validation. Please try again with a different value. '
     end
-
-    return validate_au(record) if record.country.downcase == 'au'
-    return validate_ca(record) if record.country.downcase == 'ca'
-
-    validate_in(record) if record.country.downcase == 'in'
   end
 
   private
 
   def validate_au(record)
-    unless au_abn?(record) || au_acn?(record)
-      return record.errors.add 'Australian tax identifications are either 9 or 11 numbers long, and only digits. Please verify.'
-    end
+
+    return add_invalid_au_error(record) unless au_abn?(@normalized_number) || au_acn?(@normalized_number)
 
     record.valid = true
-
-    if au_abn?(record)
-      record.tin_type = :au_abn
-
-      return record.formatted_tin = "#{record.number[0..1]} #{record.number[2..4]} #{record.number[5..7]} #{record.number[8..10]}"
-    end
-
-    record.tin_type = :au_acn
-    record.formatted_tin = "#{@tin[0..2]} #{@tin[3..5]} #{@tin[6..8]}"
+    record.tin_type, record.formatted_tin = determine_au_tin_variation(@normalized_number)
   end
 
   def validate_ca(record)
-    record
+    return add_invalid_ca_error(record) unless ca_gst?(@normalized_number)
+
+    record.valid = true
+    record.tin_type = :ca_gst
+    record.formatted_tin = "#{@normalized_number[0..8]}RT0001"
   end
 
   def validate_in(record)
-    record
+    return add_invalid_in_error(record) unless in_gst?(@normalized_number)
+
+    record.valid = true
+    record.tin_type = :ca_gst
+    record.formatted_tin = "#{@normalized_number[0..8]}RT0001"
   end
 
-  def au_abn?(record)
-    record.number.match?(/^\d{11}$/)
+  def determine_au_tin_variation(number)
+    if number.length == 11
+      [:au_abn, "#{number[0..1]} #{number[2..4]} #{number[5..7]} #{number[8..10]}"]
+    else
+      [:au_acn, "#{number[0..2]} #{number[3..5]} #{number[6..8]}"]
+    end
   end
 
-  def au_acn?(record)
-    record.number.match?(/^\d{9}$/)
+  def au_abn?(number)
+    number.match?(/^\d{11}$/)
   end
 
-  def ca_gst?(record)
-    record.number.match?(/^\d{10}$/)
+  def au_acn?(number)
+    number.match?(/^\d{9}$/)
   end
 
-  def in_gst?(record)
-    record.number.match?(/\d{2}[A-Z0-9]{10}\d[A-Z]\d/)
+  def ca_gst?(number)
+    number.match?(/^\d{9}(RT0001)?$/)
+  end
+
+  def in_gst?(number)
+    number.match?(/\d{2}[A-Z0-9]{10}\d[A-Z]\d/)
+  end
+
+  def add_invalid_au_error(record)
+    record.errors.add 'Australian tax identifications are either 9 or 11 numbers long, and only digits. Please verify.'
+  end
+
+  def add_invalid_ca_error(record)
+    record.errors.add 'Canadian tax identifications are 9 numbers long ending in RT0001 (input the numbers alone). Please verify.'
+  end
+
+  def add_invalid_in_error(record)
+    record.errors.add 'Indian tax identifications are 15 digits: 2 numbers, 10 alphanumerics, 1 number, 1 letter and 1 number long. Please verify.'
   end
 end
-
-
-
-
-
-# RULES = {
-#   'au' => {
-#     au_abn: [/^\d{11}$/, 11],
-#     au_acn: [/^\d{9}$/, 9],
-#   },
-#   'ca' =>{
-#     ca_gst: [/^\d{9}$/, 9]
-#   },
-#   'in' =>{
-#     in_gst: [/\d{2}[A-Z0-9]{10}\d[A-Z]\d/, 15]
-#   }
-# }
