072dac9e2d9cb6a61539bafd747a99baafac0a02
ejaramillom <emmanuel@knakai.com>
2025-02-22 08:41:00 -0500

    feat(PT3): Complete implementation of client to simulate external connection

--- app/clients/gst_client.rb
+++ app/clients/gst_client.rb
@@ -17,16 +17,34 @@ class GstClient
     uri = URI("http://localhost:8080/queryABN?abn=#{record.number}")
     headers = { 'Content-Type' => 'application/json' }
     response = Net::HTTP.get_response(uri, headers)
+    parsed_body = build_business_properties(response)
 
-    parse_response(response)
+    parse_response(parsed_body)
   end
 
-  def parse_response(response)
-    case response.code
-    when '200' then { body: response.body, error: nil }
-    when '404' then { body: response.body, error: 'Business is not registered' }
-    when '500' then { body: response.body, error: 'Registration API could not be reached' }
-    else { body: response.body, error: 'Unknown error' }
+  def parse_response(business)
+    case business[:code]
+    when '200' then { body: business[:business_registration], error: nil }
+    when '404' then { body: business[:business_registration], error: 'Business is not registered' }
+    when '500' then { body: business[:business_registration], error: 'Registration API could not be reached' }
+    else { body: business[:business_registration], error: 'Unknown error' }
     end
+  end
+
+  def build_business_properties(response)
+    code = response.code
+    properties = Nokogiri::XML(response.body)
+
+    name = properties.at_xpath('//businessEntity/organisationName').content
+    state = properties.at_xpath('//businessEntity/address/stateCode').content
+    postcode = properties.at_xpath('//businessEntity/address/postcode').content
+
+    {
+      code: code,
+      business_registration: {
+        name: name,
+        address: "#{state}, #{postcode}"
+      }
+    }
   end
 end
